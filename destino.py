6d494ac21a175d2ccf76a13f312d72b2bd2d309c
Emanuel7864 <em.linco@duocuc.cl>
2024-07-09 00:44:14 +0000

    archivo para el item 2

---
+++ destino.py
@@ -0,0 +1,102 @@
+import requests
+import urllib.parse
+
+route_url = "https://graphhopper.com/api/1/route?"
+key = "304feee4-a9e5-4e47-9ad9-e7686b0dcb95"
+
+def geocoding (location, key):
+    while location == "":
+        location = input("Enter the location again: ")
+    geocode_url = "https://graphhopper.com/api/1/geocode?" 
+    url = geocode_url + urllib.parse.urlencode({"q":location, "limit": "1", "key":key})
+
+    replydata = requests.get(url)
+    json_data = replydata.json()
+    json_status = replydata.status_code
+
+    if json_status == 200 and len(json_data["hits"]) != 0:
+        json_data = requests.get(url).json()
+        lat = json_data["hits"][0]["point"]["lat"]
+        lng = json_data["hits"][0]["point"]["lng"]
+        name = json_data["hits"][0]["name"]
+        value = json_data["hits"][0]["osm_value"]
+        
+        if "country" in json_data["hits"][0]:
+            country = json_data["hits"][0]["country"]
+        else:
+            country=""
+        
+        if "state" in json_data["hits"][0]:
+            state = json_data["hits"][0]["state"]
+        else:
+            state=""
+        
+        if len(state) !=0 and len(country) !=0:
+            new_loc = name + ", " + state + ", " + country
+        elif len(state) !=0:
+            new_loc = name + ", " + country
+        else:
+            new_loc = name
+        
+        print("Geocoding API URL for " + new_loc + " (Location Type: " + value + ")\n" + url)
+    else:
+        lat="null"
+        lng="null"
+        new_loc=location
+        if json_status != 200:
+            print("Geocode API status: " + str(json_status) + "\nError message: " + json_data["message"])        
+    return json_status,lat,lng,new_loc
+
+while True:
+    print("\n+++++++++++++++++++++++++++++++++++++++++++++")
+    print("Vehicle profiles available on Graphhopper:")
+    print("+++++++++++++++++++++++++++++++++++++++++++++")
+    print("car, bike, foot")
+    print("+++++++++++++++++++++++++++++++++++++++++++++")
+    profile=["car", "bike", "foot"]
+    vehicle = input("Ingresa la forma de movilizarce de la siguiente lista: ")
+    if vehicle == "salir" or vehicle == "s":
+        break
+    elif vehicle in profile:
+        vehicle = vehicle
+    else: 
+        vehicle = "car"
+        print("el vehiculo seleccionado no se encuentra en la lista.")
+
+    loc1 = input("localizacion inicial: ")
+    if loc1 == "quit" or loc1 == "s":
+        break
+    orig = geocoding(loc1, key)
+    print(orig)
+    loc2 = input("Destino: ") 
+    if loc2 == "quit" or loc2 == "s":
+        break
+    dest = geocoding(loc2, key)
+    print("=================================================")
+    if orig[0] == 200 and dest[0] == 200:
+        op="&point="+str(orig[1])+"%2C"+str(orig[2])
+        dp="&point="+str(dest[1])+"%2C"+str(dest[2])
+        paths_url = route_url + urllib.parse.urlencode({"key":key, "vehicle":vehicle}) + op + dp
+        paths_status = requests.get(paths_url).status_code
+        paths_data = requests.get(paths_url).json()
+        print("Routing API Status: " + str(paths_status) + "\nRouting API URL:\n" + paths_url)
+        print("=================================================")
+        print("Directions from " + orig[3] + " to " + dest[3] + " by " + vehicle)
+        print("=================================================")
+        if paths_status == 200:
+            millas = (paths_data["paths"][0]["distance"])/1000/1.61
+            km = (paths_data["paths"][0]["distance"])/1000
+            sec = int(paths_data["paths"][0]["time"]/1000%60)
+            min = int(paths_data["paths"][0]["time"]/1000/60%60)
+            hr = int(paths_data["paths"][0]["time"]/1000/60/60)
+            print("Distancia: {0:.1f} millas / {1:.1f} km".format(millas, km))
+            print("duracion del viaje: {0:02d}:{1:02d}:{2:02d}".format(hr, min, sec))
+            print("=================================================")
+            for each in range(len(paths_data["paths"][0]["instructions"])):
+                path = paths_data["paths"][0]["instructions"][each]["text"]
+                Distancia = paths_data["paths"][0]["instructions"][each]["distance"]
+                print("{0} ( {1:.1f} km / {2:.1f} millas )".format(path, Distancia/1000, Distancia/1000/1.61))
+            print("=============================================")
+        else:
+            print("Error message: " + paths_data["message"])
+            print("*************************************************")
